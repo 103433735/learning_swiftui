710664c599ad808e5288b35829150a01c5cb9f41
DucNguyen <ducnafs1579@gmail.com>
2024-10-02 16:51:01 +0700

    Backup code

---
+++ LearningSwiftUI/IconGrid.swift
@@ -0,0 +1,18 @@
+//
+//  IconGrid.swift
+//  LearningSwiftUI
+//
+//  Created by Hartzed Story on 2/10/24.
+//
+
+import SwiftUI
+
+struct IconGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconGrid()
+}
---
+++ LearningSwiftUI/Model/Currency.swift
@@ -0,0 +1,8 @@
+//
+//  Currency.swift
+//  LearningSwiftUI
+//
+//  Created by Hartzed Story on 2/10/24.
+//
+
+import Foundation
